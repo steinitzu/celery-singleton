581a308a33ec3acc321b9f62526ab69dd51b7b5a
Steinthor Palsson <steini90@gmail.com>
2017-08-02 13:38:47 -0400

    version bump to 0.1.3 -> fix error when `get_existing_task_id` called on non-existent lock

--- celery_singleton/__init__.py
+++ celery_singleton/__init__.py
@@ -1,3 +1,3 @@
 from .singleton import Singleton, clear_locks
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='celery-singleton',
-    version='0.1.2',
+    version='0.1.3',
     description='Prevent duplicate celery tasks',
     author='Steinthor Palsson',
     author_email='steini90@gmail.com',
